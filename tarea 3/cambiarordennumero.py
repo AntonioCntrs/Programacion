7117816f7a308278339a89913869ea5559e692c8
AntonioCntrs <antoniocntrs97@gmail.com>
2024-10-02 21:14:08 -0700

    Update cambiarordennumero.py

--- tarea 3/cambiarordennumero.py
+++ tarea 3/cambiarordennumero.py
@@ -1,8 +1,8 @@
 # Contreras Osuna Jose Antonio #240053
 n=int(input("Dame un numero entero de tres digitos"))
-if 1 <= n <= 999:
+if 100 <= n <= 999:
         # Convertir el número a cadena, invertir la cadena y convertirla de nuevo a entero
      ni= int(str(n)[::-1])
 else:
-    print("El número debe estar en el rango de 1 a 999.")
+    print("El número debe ser de tres digitos")
 print(f"El numero invertido de {n} es {ni}")
